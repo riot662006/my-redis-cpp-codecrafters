89d5220171da878a5a8618373cc82ac5e546e1a8
riot662006 <ibekwe2006@gmail.com>
2024-03-25 11:47:36 -0500

    Implemented multiple ping response

--- src/Server.cpp
+++ src/Server.cpp
@@ -9,9 +9,7 @@
 #include <netdb.h>
 
 int main(int argc, char **argv) {
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
-  std::cout << "Logs from your program will appear here!\n";
-
+  // Server socket initializer
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0) {
    std::cerr << "Failed to create server socket\n";
@@ -40,21 +38,31 @@ int main(int argc, char **argv) {
     std::cerr << "listen failed\n";
     return 1;
   }
-  
-  struct sockaddr_in client_addr;
+  // at this point, the server is on
+
+  struct sockaddr_in client_addr; // for the single client
   int client_addr_len = sizeof(client_addr);
   
   std::cout << "Waiting for a client to connect...\n";
   
   int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-  std::cout << "Client connected\n";
+  std::cout << "Client connected\n"; // at this point, the client has connected
 
-  char wbuf[1024] = {0};
+  char rbuf[1024] = {0}; // read buffer... to receive data from client
 
-  read(client_fd, wbuf, 1024);
+  while (true) {
+    int n = read(client_fd, rbuf, 1024);
 
-  if (memcmp(wbuf, "*1\r\n$4\r\nping\r\n", 15) == 0) {
-    write(client_fd, "+PONG\r\n", 7);
+    if (n == 0) break;
+    
+    while (n > 0) {
+      if (n >= 14) {
+        if (memcmp(rbuf, "*1\r\n$4\r\nping\r\n", 14) == 0) { // checks if the buffer has the ping command
+          write(client_fd, "+PONG\r\n", 7); // sends PONG response
+          n -= 14;
+        }
+      }
+    }
   }
   
   close(server_fd);
